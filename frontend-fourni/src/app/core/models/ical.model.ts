18ae63acf56f8bc2f93a01fc9eea0fb0ce4e4eb6
Souleye <sulaynd@gmail.com>
2026-07-22 13:40:45 -0400

    Retirer les fichiers de test/scripts obsolètes du dépôt

--- frontend-fourni/src/app/core/models/ical.model.ts
+++
@@ -1,4 +0,0 @@
-export interface LiensIcal {
-  personnel: string;
-  equipe?: string;
-}
--- frontend-fourni/src/app/core/models/portail-client.model.ts
+++
@@ -1,8 +0,0 @@
-export interface ClientPortail {
-  id: number;
-  type: 'particulier' | 'entreprise';
-  nom?: string;
-  prenom?: string;
-  raison_sociale?: string;
-  email: string;
-}
